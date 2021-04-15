7a48f8d6f050bd403a34b1f5332eecb4d502bf7b
maddieeavery44 <82195893+maddieeavery44@users.noreply.github.com>
2021-04-15 06:14:43 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -22,7 +22,7 @@ function createPhotoMap () {
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
   var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   }).addTo(map);
   
   // use Papa Parse (papaparse.com) to get the Google Sheets CSV
